e4ea571b45570ba807efb97befb0950d10ede2ec
hanchee <hancheester@gmail.com>
2017-03-06 11:54:01 +0000

    Removed files from src.

--- src/BugzillaAPI.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TurtleZilla
-{
-    public class BugzillaAPI
-    {
-        public const string VERSION = "/rest/version";
-        public const string BUG = "/rest/bug";
-        public const string PRODUCT = "/rest/product";
-    }
-}
